53ac357c45d14b717c2b2575c012f2adea88e50c
Arlindo Honorato Pereira Júnior <junior_honorato@live.com>
2026-03-08 18:10:10 -0300

    Create executar_robo.bat

---
+++ executar_robo.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Iniciando o Robo da Azul...
+
+REM Altere o caminho abaixo para onde o Python esta instalado e onde o script esta salvo
+"C:\Caminho\Para\Python\python.exe" "C:\Caminho\Para\Sua\Pasta\azul_bot.py"
+
+echo Execucao finalizada.
